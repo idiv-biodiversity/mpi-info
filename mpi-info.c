d73c80231c77c6f91b2f44b6ab8d8049df904109
Christian Krause <kizkizzbangbang@googlemail.com>
2013-06-21 14:29:35 +0200

    every rank prints virtual memory limit

--- mpi-info.c
+++ mpi-info.c
@@ -1,12 +1,16 @@
+#include <errno.h>
+#include <sys/resource.h>
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
   int mpi_e, mpi_e_msg_s, mpi_s, mpi_r = 0;
   int sum = 0;
   char procname[MPI_MAX_PROCESSOR_NAME];
   char mpi_e_msg[MPI_MAX_ERROR_STRING];
+  struct rlimit limit;
 
   // ------------------------------------------------------------------------------------------------
   // MPI_Init
@@ -56,6 +60,16 @@ int main(int argc, char **argv) {
   // ------------------------------------------------------------------------------------------------
 
   printf("[INFO] [%s] [%d] MPI_Comm_size(MPI_COMM_WORLD): %d\n", procname, mpi_r, mpi_s);
+
+  // ------------------------------------------------------------------------------------------------
+  // resource usage
+  // ------------------------------------------------------------------------------------------------
+
+  if (getrlimit(RLIMIT_AS, &limit) == 0) {
+    printf("[INFO] [%s] [%d] virtual memory limit (RLIMIT_AS): %lu\n", procname, mpi_r, limit.rlim_max);
+  } else {
+    printf("[INFO] [%s] [%d] %d = getrlimit(%d): %s\n", procname, mpi_r, errno, RLIMIT_AS, strerror(errno));
+  }
 
   // ------------------------------------------------------------------------------------------------
   // MPI_Allreduce
